8c440d5a9f7a8c3544fc4597ebc9b7388129a75b
RoodAnchor <ujcn2086@gmail.com>
2024-02-18 14:09:26 +0300

    Добавил комменты

--- UnitTesting.Tests/CalculatorTests.cs
+++ UnitTesting.Tests/CalculatorTests.cs
@@ -2,27 +2,45 @@
 
 namespace UnitTesting.Tests
 {
+    /// <summary>
+    /// Юнит тесты калькулятора
+    /// </summary>
     [TestFixture]
     public class CalculatorTests
     {
         private Calculator _calculator = new Calculator();
-
+        
+        /// <summary>
+        /// Тест на корректность сложения
+        /// </summary>
         [Test]
         public void AdditionShouldReturnCorrectValue() =>
             Assert.That(_calculator.Addition(2, 3) == 5);
 
+        /// <summary>
+        /// Тест на корректность вычитания
+        /// </summary>
         [Test]
         public void SubtractionShouldReturnCorrectValue() =>
             Assert.That(_calculator.Subtraction(5, 3) == 2);
 
+        /// <summary>
+        /// Тест на корректность умножения
+        /// </summary>
         [Test]
         public void MultiplicationShouldReturnCorrectValue() =>
             Assert.That(_calculator.Multiplication(2, 3) == 6);
 
+        /// <summary>
+        /// Тест на корректность деления
+        /// </summary>
         [Test]
         public void DivisionShouldReturnCorrectValue() =>
             Assert.That(_calculator.Division(6, 3) == 2);
 
+        /// <summary>
+        /// Тест на исключение деления на ноль.
+        /// </summary>
         [Test]
         public void DivisionShouldThrowDivisionByZeroException() =>
             Assert.Throws(typeof(DivideByZeroException), () => _calculator.Division(2, 0));
--- UnitTesting/Calculator.cs
+++ UnitTesting/Calculator.cs
@@ -1,16 +1,43 @@
 ﻿namespace UnitTesting
 {
+    /// <summary>
+    /// Калькулятор
+    /// </summary>
     public class Calculator
     {
+        /// <summary>
+        /// Метод нахождения суммы 2 чисел
+        /// </summary>
+        /// <param name="a">Первое слагаемое</param>
+        /// <param name="b">Второе слагаемое</param>
+        /// <returns>Сумму чисел</returns>
         public Int32 Addition(Int32 a, Int32 b) =>
             a + b;
 
+        /// <summary>
+        /// Метод нахождения разности 2 чисел
+        /// </summary>
+        /// <param name="a">Уменьшаемое</param>
+        /// <param name="b">Вычитаемое</param>
+        /// <returns>Разность чисел</returns>
         public Int32 Subtraction(Int32 a, Int32 b) =>
             a - b;
 
+        /// <summary>
+        /// Метод нахождения произведения 2 чисел
+        /// </summary>
+        /// <param name="a">Первый множитель</param>
+        /// <param name="b">Второй множитель</param>
+        /// <returns>Произведение чисел</returns>
         public Int32 Multiplication(Int32 a, Int32 b) =>
             a * b;
 
+        /// <summary>
+        /// Метод нахождения частного 2 чисел
+        /// </summary>
+        /// <param name="a">Делимое</param>
+        /// <param name="b">Делитель</param>
+        /// <returns>Частное чисел</returns>
         public Int32 Division(Int32 a, Int32 b) =>
             a / b;
     }

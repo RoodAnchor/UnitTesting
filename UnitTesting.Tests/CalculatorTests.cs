88ad0c5f97f9241e4a5bb1f5dfe3d8e519ea4419
RoodAnchor <ujcn2086@gmail.com>
2024-02-18 13:57:37 +0300

    Initial commit

---
+++ UnitTesting.Tests/CalculatorTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace UnitTesting.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator _calculator = new Calculator();
+
+        [Test]
+        public void AdditionShouldReturnCorrectValue() =>
+            Assert.That(_calculator.Addition(2, 3) == 5);
+
+        [Test]
+        public void SubtractionShouldReturnCorrectValue() =>
+            Assert.That(_calculator.Subtraction(5, 3) == 2);
+
+        [Test]
+        public void MultiplicationShouldReturnCorrectValue() =>
+            Assert.That(_calculator.Multiplication(2, 3) == 6);
+
+        [Test]
+        public void DivisionShouldReturnCorrectValue() =>
+            Assert.That(_calculator.Division(6, 3) == 2);
+
+        [Test]
+        public void DivisionShouldThrowDivisionByZeroException() =>
+            Assert.Throws(typeof(DivideByZeroException), () => _calculator.Division(2, 0));
+    }
+}
---
+++ UnitTesting/Calculator.cs
@@ -0,0 +1,17 @@
+﻿namespace UnitTesting
+{
+    public class Calculator
+    {
+        public Int32 Addition(Int32 a, Int32 b) =>
+            a + b;
+
+        public Int32 Subtraction(Int32 a, Int32 b) =>
+            a - b;
+
+        public Int32 Multiplication(Int32 a, Int32 b) =>
+            a * b;
+
+        public Int32 Division(Int32 a, Int32 b) =>
+            a / b;
+    }
+}
